32fa8197e4de29f4d31771e33e0c40fc5a35f8f2
Valentin Lorentz <progval@progval.net>
2014-12-23 23:02:54 +0100

    Add more models (all?)

--- enseigner/model.py
+++ enseigner/model.py
@@ -6,6 +6,8 @@ import weakref
 
 class NotFound(Exception):
     pass
+class ForeignKeyNotMapped(Exception):
+    pass
 class Duplicate(Exception):
     pass
 class Contradictory(Exception):
@@ -18,7 +20,7 @@ def register(cls):
 
 
 def password_hash(tutor_email, password):
-    salt = os.environ.get('ENSEIGNER_SECRET_SALT', '')
+    salt = os.environ.get('ENSEIGNER_PASSWORD_SALT', '')
     assert len(salt) >= 20, 'Salt is not long enough'
     return hashlib.sha512('%s|%s|%s' % (salt, tutor_email, password)).hexdigest()
 
@@ -26,40 +28,76 @@ def password_hash(tutor_email, password):
 def get_conn(): # pragma: no cover
     return sqlite3.connect('database.sqlite3')
 
-@register
-class Tutor(object):
-    _create_table = '''CREATE TABLE tutors (
-        tutor_id INTEGER PRIMARY KEY,
-        tutor_email TEXT UNIQUE,
-        tutor_password_hash TEXT,
-        tutor_is_admin BOOLEAN
-        )'''
-    _instances = weakref.WeakValueDictionary()
-    _fields = ('uid', 'email', 'password_hash', 'is_admin')
-    def __init__(self, uid, email, password_hash, is_admin):
-        if uid in self._instances:
-            assert self._instances[uid] is self
-        else:
-            self._instances[uid] = self
-        self.uid = uid
-        self.email = email
-        self.password_hash = password_hash
-        self.is_admin = is_admin
+def _model(nb_keys):
+    class Model(object):
+        @staticmethod
+        def make_id(args):
+            return tuple(args[0:nb_keys])
+        def __init__(self, *args):
+            id_ = self.make_id(args)
+            if id_ in self._instances:
+                assert self._instances[id_] is self
+            else:
+                self._instances[id_] = self
+                for (key, value) in zip(self._fields, args):
+                    setattr(self, key, value)
 
-    def __repr__(self):
-        return '<enseigner.model.Tutor(%s)>' % ', '.join(
-                ['%s=%r' % (x, getattr(self, x)) for x in self._fields])
+        def __repr__(self):
+            return '<enseigner.model.%s(%s)>' % (self.__class__.__name__,
+                    ', '.join(['%s=%r' % (x, getattr(self, x))
+                               for x in self._fields]))
+
+        @classmethod
+        def _check_exists(cls, cls2, key):
+            try:
+                cls2.get(key)
+            except NotFound:
+                raise ForeignKeyNotMapped()
+
+        @classmethod
+        def _get_or_create(cls, data):
+            if not data:
+                raise NotFound()
+            id_ = cls.make_id(data)
+            if id_ in cls._instances:
+                return cls._instances[id_]
+            else:
+                instance = cls(*data)
+                cls._instances[id_] = instance
+                return instance 
+
+        @classmethod
+        def _fetch_many(cls, request, args=()):
+            conn = get_conn()
+            c = conn.cursor()
+            try:
+                c.execute(request, args)
+                r = c.fetchall()
+            finally:
+                c.close()
+            return r
+
+        @classmethod
+        def _get_many(cls, request, args=()):
+            r = cls._fetch_many(request, args)
+            return map(lambda x:cls._instances.get(cls.make_id(x), None) or 
+                                cls(*x), r)
+
+    Model.__name__ = 'Model(%d)' % nb_keys
+    return Model
+
+class SingleKeyModel(_model(1)):
+    @classmethod
+    def make_id(self, args):
+        return args[0]
 
     @classmethod
-    def create(cls, email, password, is_admin):
+    def _insert_one(cls, cols, args):
         conn = get_conn()
         c = conn.cursor()
-        hash_ = password_hash(email, password)
         try:
-            c.execute('''INSERT INTO tutors
-                         (tutor_email, tutor_password_hash, tutor_is_admin)
-                         VALUES (?, ?, ?)''',
-                      (email, hash_, is_admin))
+            c.execute('INSERT INTO %s (%s) VALUES (%s)' %
+                      (cls._table, cols, ', '.join('?'*len(args))), args)
             r = c.lastrowid
         except sqlite3.IntegrityError:
             raise Duplicate()
@@ -67,7 +105,31 @@ class Tutor(object):
             conn.commit()
         finally:
             c.close()
-        return Tutor(r, email, hash_, is_admin)
+        return cls(r, *args)
+DoubleKeyModel = _model(2)
+TripleKeyModel = _model(3)
+
+@register
+class Tutor(SingleKeyModel):
+    _table = 'tutors'
+    _create_table = '''CREATE TABLE tutors (
+        tutor_id INTEGER PRIMARY KEY,
+        tutor_email TEXT UNIQUE,
+        tutor_password_hash TEXT,
+        tutor_phone_number TEXT,
+        tutor_is_admin BOOLEAN,
+        tutor_is_active BOOLEAN,
+        tutor_comment TEXT
+        )'''
+    _instances = weakref.WeakValueDictionary()
+    _fields = ('uid', 'email', 'password_hash', 'phone_number', 'is_admin', 'is_active', 'comment')
+
+    @classmethod
+    def create(cls, email, password, phone_number=None, is_admin=False, is_active=True, comment=None):
+        hash_ = password_hash(email, password)
+        return cls._insert_one('''tutor_email, tutor_password_hash, tutor_phone_number,
+                                  tutor_is_admin, tutor_is_active, tutor_comment''',
+                              (email, hash_, phone_number, is_admin, is_active, comment))
 
         
     @classmethod
@@ -89,25 +151,11 @@ class Tutor(object):
             r = c.fetchone()
         finally:
             c.close()
-        if not r:
-            raise NotFound()
-        if r[0] in cls._instances:
-            return cls._instances[r[0]]
-        else:
-            instance = cls(*r)
-            cls._instances[r[0]] = instance
-            return instance 
+        return cls._get_or_create(r)
 
     @classmethod
-    def get_tutors(cls):
-        conn = get_conn()
-        c = conn.cursor()
-        try:
-            c.execute('''SELECT * FROM tutors''')
-            r = c.fetchall()
-        finally:
-            c.close()
-        return map(lambda x:cls._instances.get(x[0], None) or cls(*x), r)
+    def all(cls):
+        return cls._get_many('''SELECT * FROM tutors''')
 
 
     @classmethod
@@ -126,3 +174,301 @@ class Tutor(object):
         else:
             return None
 
+
+@register
+class Student(SingleKeyModel):
+    _table = 'students'
+    _create_table = '''CREATE TABLE students (
+        student_id INTEGER PRIMARY KEY,
+        student_emails TEXT,
+        student_name TEXT UNIQUE,
+        student_phone_number TEXT,
+        student_is_active BOOLEAN,
+        student_blacklisted BOOLEAN,
+        student_comment TEXT
+        )'''
+    _instances = weakref.WeakValueDictionary()
+    _fields = ('uid', 'emails', 'name', 'is_active', 'blacklisted', 'comment')
+
+    @classmethod
+    def create(cls, emails, name, phone_number=None, is_active=True, blacklisted=False, comment=None):
+        return cls._insert_one('''student_emails, student_name, student_phone_number,
+                                  student_is_active, student_blacklisted, student_comment''',
+                               (emails, name, phone_number, is_active, blacklisted, comment))
+
+        
+    @classmethod
+    def get(cls, uid):
+        conn = get_conn()
+        c = conn.cursor()
+        try:
+            if not isinstance(uid, int):
+                raise ValueError('id should be or int, not %r' %
+                        uid)
+            c.execute('''SELECT * FROM students
+                         WHERE student_id=?''',
+                      (uid,))
+            r = c.fetchone()
+        finally:
+            c.close()
+        return cls._get_or_create(r)
+
+    @classmethod
+    def all(cls):
+        return cls._get_many('''SELECT * FROM students''')
+
+@register
+class Session(SingleKeyModel):
+    _table = 'sessions'
+    _create_table = '''CREATE TABLE sessions (
+        session_id INTEGER PRIMARY KEY,
+        session_date DATETIME,
+        session_managers TEXT
+        )'''
+    _instances = weakref.WeakValueDictionary()
+    _fields = ('sid', 'date', 'managers')
+
+    @classmethod
+    def create(cls, *args):
+        return cls._insert_one('session_date, session_managers', args)
+
+    @classmethod
+    def get(cls, sid):
+        assert isinstance(sid, int)
+        conn = get_conn()
+        c = conn.cursor()
+        try:
+            c.execute('''SELECT * FROM sessions
+                         WHERE session_id=?''',
+                      (sid,))
+            r = c.fetchone()
+        finally:
+            c.close()
+        return cls._get_or_create(r)
+
+    @classmethod
+    def all(cls):
+        return cls._get_many('''SELECT * FROM sessions''')
+
+
+@register
+class TutorRegistration(SingleKeyModel):
+    _table = 'tutor_registrations'
+    _create_table = '''CREATE TABLE tutor_registrations (
+        treg_id INTEGER PRIMARY KEY,
+        treg_session_id INTEGER,
+        treg_tutor_id INTEGER,
+        treg_group_size INTEGER,
+        treg_comment TEXT,
+        FOREIGN KEY (treg_session_id) REFERENCES sessions(session_id),
+        FOREIGN KEY (treg_tutor_id) REFERENCES tutors(tutor_id),
+        UNIQUE (treg_session_id, treg_tutor_id)
+        )'''
+    _instances = weakref.WeakValueDictionary()
+    _fields = ('trid', 'sid', 'uid', 'group_size', 'comment')
+
+    @classmethod
+    def create(cls, session, tutor, *args):
+        if isinstance(session, Session):
+            session = session.sid
+        else:
+            cls._check_exists(Session, session)
+        if isinstance(tutor, Tutor):
+            tutor = tutor.uid
+        else:
+            cls._check_exists(Tutor, tutor)
+        return cls._insert_one('''treg_session_id, treg_tutor_id,
+                                  treg_group_size, treg_comment''',
+                               (session, tutor) + args)
+
+    @classmethod
+    def all_in_session(cls, session):
+        if isinstance(session, Session):
+            session = session.sid
+        assert isinstance(session, int), session
+        return cls._get_many('''SELECT * FROM tutor_registrations
+                                WHERE treg_session_id=?''', (session,))
+
+    @classmethod
+    def get(cls, trid):
+        conn = get_conn()
+        c = conn.cursor()
+        try:
+            c.execute('''SELECT * FROM tutor_registrations
+                         WHERE treg_id=?''',
+                      (trid,))
+            r = c.fetchone()
+        finally:
+            c.close()
+        return cls._get_or_create(r)
+
+    @classmethod
+    def find(cls, session, tutor):
+        if isinstance(session, Session):
+            session = session.sid
+        else:
+            cls._check_exists(Session, session)
+        if isinstance(tutor, Tutor):
+            tutor = tutor.uid
+        else:
+            cls._check_exists(Tutor, tutor)
+        r = cls._get_many('''SELECT * FROM tutor_registrations
+                             WHERE treg_session_id=? AND treg_tutor_id=?''',
+                          (session, tutor))
+        r = list(r)
+        if not r:
+            raise NotFound()
+        else:
+            assert len(r) == 1
+            return r[0]
+
+@register
+class Subject(SingleKeyModel):
+    _table = 'subjects'
+    _create_table = '''CREATE TABLE subjects (
+        subject_id INTEGER PRIMARY KEY,
+        subject_name TEXT UNIQUE,
+        subject_is_exceptional BOOLEAN
+        )'''
+    _instances = weakref.WeakValueDictionary()
+    _fields = ('sid', 'name', 'is_exceptional')
+
+    @classmethod
+    def create(cls, *args):
+        return cls._insert_one('subject_name, subject_is_exceptional', args)
+        
+    @classmethod
+    def get(cls, sid):
+        conn = get_conn()
+        c = conn.cursor()
+        try:
+            if not isinstance(sid, int):
+                raise ValueError('id should be or int, not %r' %
+                        sid)
+            c.execute('''SELECT * FROM subjects
+                         WHERE subject_id=?''',
+                      (sid,))
+            r = c.fetchone()
+        finally:
+            c.close()
+        return cls._get_or_create(r)
+
+@register
+class TutorRegistrationSubject(SingleKeyModel):
+    _table = 'tutor_registrations_subject'
+    _create_table = '''CREATE TABLE tutor_registrations_subject (
+        tregs_id INTEGER PRIMARY KEY,
+        tregs_treg_id INTEGER,
+        tregs_subject_id INTEGER,
+        tregs_preference INTEGER,
+        FOREIGN KEY (tregs_treg_id) REFERENCES tutor_registrations(treg_id),
+        FOREIGN KEY (tregs_subject_id) REFERENCES subjects(subject_id),
+        UNIQUE (tregs_treg_id, tregs_subject_id)
+        )'''
+    _instances = weakref.WeakValueDictionary()
+    _fields = ('id', 'trid', 'sid', 'preference')
+
+    @classmethod
+    def create(cls, treg, subject, pref):
+        if isinstance(treg, TutorRegistration):
+            treg = treg.trid
+        else:
+            cls._check_exists(TutorRegistration, treg)
+        if isinstance(subject, Subject):
+            subject = subject.sid
+        else:
+            cls._check_exists(Subject, subject)
+        return cls._insert_one('''tregs_treg_id, tregs_subject_id,
+                                  tregs_preference''', (treg, subject, pref))
+
+    @classmethod
+    def all_of_treg(cls, treg):
+        if isinstance(treg, TutorRegistration):
+            treg = treg.trid
+        assert isinstance(treg, int), treg
+        return cls._get_many('''SELECT * FROM tutor_registrations_subject
+                                WHERE tregs_treg_id=?''', (treg,))
+
+@register
+class StudentRegistration(SingleKeyModel):
+    _table = 'student_registrations'
+    _create_table = '''CREATE TABLE student_registrations (
+        sreg_id INTEGER PRIMARY KEY,
+        sreg_session_id INTEGER,
+        sreg_student_id INTEGER,
+        sreg_subject_id INTEGER,
+        sreg_friends INTEGER,
+        sreg_comment TEXT,
+        FOREIGN KEY (sreg_session_id) REFERENCES sessions(session_id),
+        FOREIGN KEY (sreg_student_id) REFERENCES students(student_id),
+        FOREIGN KEY (sreg_subject_id) REFERENCES subjects(subject_id),
+        UNIQUE (sreg_session_id, sreg_student_id)
+        )'''
+    _fields = ('srid', 'seid', 'stid', 'suid', 'friends', 'comment')
+
+    @classmethod
+    def create(cls, session, student, subject, *args):
+        if isinstance(session, Session):
+            session = session.sid
+        else:
+            cls._check_exists(Session, session)
+        if isinstance(student, Student):
+            student = student.uid
+        else:
+            cls._check_exists(Student, student)
+        if isinstance(subject, Subject):
+            subject = subject.sid
+        else:
+            cls._check_exists(Subject, subject)
+        assert isinstance(session, int), session
+        assert isinstance(student, int), student
+        assert isinstance(subject, int), subject
+        return cls._insert_one('''sreg_session_id, sreg_student_id,
+                                  sreg_subject_id, sreg_friends, sreg_comment''',
+                               (session, student, subject) + args)
+        
+    @classmethod
+    def get(cls, sid):
+        conn = get_conn()
+        c = conn.cursor()
+        try:
+            if not isinstance(sid, int):
+                raise ValueError('id should be or int, not %r' %
+                        sid)
+            c.execute('''SELECT * FROM student_registrations
+                         WHERE sreg_id=?''',
+                      (sid,))
+            r = c.fetchone()
+        finally:
+            c.close()
+        return cls._get_or_create(r)
+
+    @classmethod
+    def all_in_session(cls, session):
+        if isinstance(session, Session):
+            session = session.sid
+        assert isinstance(session, int), session
+        return cls._get_many('''SELECT * FROM student_registrations
+                                WHERE sreg_session_id=?''', (session,))
+
+    @classmethod
+    def find(cls, session, student):
+        if isinstance(session, Session):
+            session = session.sid
+        else:
+            assert isinstance(session, int), session
+            cls._check_exists(Session, session)
+        if isinstance(student, Student):
+            student = student.uid
+        else:
+            assert isinstance(student, int), student
+            cls._check_exists(Student, student)
+        r = cls._get_many('''SELECT * FROM student_registrations
+                             WHERE sreg_session_id=? AND sreg_student_id=?''',
+                          (session, student))
+        r = list(r)
+        if not r:
+            raise NotFound()
+        else:
+            assert len(r) == 1, r
+            return r[0]
--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,7 @@ from testutils import EnseignerTestCase
 
 import enseigner.model as model
 
-class TutorTestCase(EnseignerTestCase):
+class ModelTestCase(EnseignerTestCase):
     def testSingleton(self):
         t1 = model.Tutor.create('foo', 'bar', False)
         self.assertRaises(model.Duplicate, model.Tutor.create, 'foo', 'bar', False)
@@ -15,15 +15,17 @@ class TutorTestCase(EnseignerTestCase):
         self.assertRaises(ValueError, model.Tutor.get, ('foo',))
 
     def testRepr(self):
-        t1 = model.Tutor.create('foo', 'bar', False)
+        t1 = model.Tutor.create('foo', 'bar')
         self.assertEqual(repr(t1),
                 "<enseigner.model.Tutor(uid=1, email='foo', "
-                "password_hash='%s', is_admin=False)>" %
+                "password_hash='%s', phone_number=None, is_admin=False, "
+                "is_active=True, comment=None)>" %
                 model.password_hash('foo', 'bar'))
 
+class TutorTestCase(EnseignerTestCase):
     def testGetTutors(self):
         t1 = model.Tutor.create('foo', 'bar', False)
-        self.assertEqual(list(model.Tutor.get_tutors()), [t1])
+        self.assertEqual(list(model.Tutor.all()), [t1])
 
     def testCheckPassword(self):
         t1 = model.Tutor.create('foo', 'bar', False)
@@ -34,3 +36,116 @@ class TutorTestCase(EnseignerTestCase):
         self.assertIs(t2, model.Tutor.check_password('foo2', 'bar2'))
         self.assertIs(None, model.Tutor.check_password('foo2', 'bar'))
 
+class StudentTestCase(EnseignerTestCase):
+    def testGetStudents(self):
+        s1 = model.Student.create('foo', 'bar', False, False, '')
+        s2 = model.Student.create('foo2', 'bar2', False, False, '')
+        self.assertRaises(model.Duplicate, model.Student.create, 'foo2', 'bar', True, False, 'baz')
+        self.assertEqual(list(model.Student.all()), [s1, s2])
+        self.assertIs(model.Student.get(s2.uid), s2)
+        self.assertRaises(ValueError, model.Student.get, s2.emails)
+
+class TregTestCase(EnseignerTestCase):
+    def testCheckForeignKeys(self):
+        self.assertRaises(model.ForeignKeyNotMapped,
+                model.TutorRegistration.create, 10, 10, 3, None)
+        t1 = model.Tutor.create('foo', 'bar', False)
+        self.assertRaises(model.ForeignKeyNotMapped,
+                model.TutorRegistration.create, 10, t1, 3, None)
+        s1 = model.Session.create('foo', 'bar')
+        self.assertRaises(model.ForeignKeyNotMapped,
+                model.TutorRegistration.create, s1, 10, 3, None)
+        model.TutorRegistration.create(s1, t1, 3, None)
+
+    def testAllInSession(self):
+        s1 = model.Session.create('foo', 'bar')
+        s2 = model.Session.create('foo2', 'bar2')
+        t1 = model.Tutor.create('foo', 'bar', False)
+        t2 = model.Tutor.create('foo2', 'bar', False)
+        t3 = model.Tutor.create('foo3', 'bar', False)
+        tr1 = model.TutorRegistration.create(s1, t1, 3, None)
+        tr2 = model.TutorRegistration.create(s1, t3, 3, None)
+        tr3 = model.TutorRegistration.create(s2, t3, 3, None)
+        self.assertEqual(set(model.TutorRegistration.all_in_session(s1)),
+                         {tr1, tr2})
+        self.assertEqual(set(model.TutorRegistration.all_in_session(s2)),
+                         {tr3})
+
+    def testGetFind(self):
+        s1 = model.Session.create('foo', 'bar')
+        s2 = model.Session.create('foo2', 'bar2')
+        t1 = model.Tutor.create('foo', 'bar', False)
+        t2 = model.Tutor.create('foo2', 'bar', False)
+        t3 = model.Tutor.create('foo3', 'bar', False)
+        tr1 = model.TutorRegistration.create(s1, t1, 3, None)
+        tr2 = model.TutorRegistration.create(s1, t3, 3, None)
+        tr3 = model.TutorRegistration.create(s2, t3, 3, None)
+        self.assertIs(model.TutorRegistration.find(s1, t3), tr2)
+        self.assertIs(model.TutorRegistration.find(s2, t3), tr3)
+        self.assertIs(model.TutorRegistration.find(s2.sid, t3.uid), tr3)
+        self.assertRaises(model.NotFound, model.TutorRegistration.find, s2, t2)
+        self.assertRaises(model.ForeignKeyNotMapped, model.TutorRegistration.find, 50, t2)
+        self.assertRaises(model.ForeignKeyNotMapped, model.TutorRegistration.find, s2, 50)
+        self.assertIs(model.TutorRegistration.get(tr2.trid), tr2)
+        self.assertRaises(model.NotFound, model.TutorRegistration.get, 100)
+
+    def testSubjects(self):
+        s1 = model.Session.create('foo', 'bar')
+        s2 = model.Session.create('foo2', 'bar2')
+        t1 = model.Tutor.create('foo', 'bar', False)
+        t2 = model.Tutor.create('foo2', 'bar', False)
+        t3 = model.Tutor.create('foo3', 'bar', False)
+        tr1 = model.TutorRegistration.create(s1, t1, 3, None)
+        tr2 = model.TutorRegistration.create(s1, t3, 3, None)
+        tr3 = model.TutorRegistration.create(s2, t3, 3, None)
+        sub1 = model.Subject.create('foo', False)
+        sub2 = model.Subject.create('bar', False)
+        sub3 = model.Subject.create('baz', False)
+        ts1 = model.TutorRegistrationSubject.create(tr1, sub2, 1)
+        ts2 = model.TutorRegistrationSubject.create(tr1.trid, sub3, 2)
+        ts3 = model.TutorRegistrationSubject.create(tr2, sub2.sid, 3)
+        self.assertEqual(set(model.TutorRegistrationSubject.all_of_treg(tr1)),
+                {ts1, ts2})
+        self.assertEqual(set(model.TutorRegistrationSubject.all_of_treg(tr2)),
+                {ts3})
+
+class SregTestCase(EnseignerTestCase):
+    def testGetFindAll(self):
+        s1 = model.Session.create('foo', 'bar')
+        s2 = model.Session.create('foo2', 'bar2')
+        st1 = model.Student.create('foo', 'bar', False, False, '')
+        st2 = model.Student.create('foo2', 'bar2', False, False, '')
+        st3 = model.Student.create('foo3', 'bar3', False, False, '')
+        sub1 = model.Subject.create('foo', False)
+        sub2 = model.Subject.create('bar', False)
+        sub3 = model.Subject.create('baz', False)
+        sr1 = model.StudentRegistration.create(s1, st1, sub1, 1, None)
+        sr2 = model.StudentRegistration.create(s1, st3, sub3.sid, 1,None)
+        sr3 = model.StudentRegistration.create(s2, st3.uid, sub2, 1, None)
+        sr4 = model.StudentRegistration.create(s2.sid, st1, sub2, 1, None)
+        self.assertRaises(model.Duplicate,
+                model.StudentRegistration.create, s1, st1, sub2, 2, None)
+        self.assertEqual(set(model.StudentRegistration.all_in_session(s1)),
+                {sr1, sr2})
+        self.assertEqual(set(model.StudentRegistration.all_in_session(s1.sid)),
+                {sr1, sr2})
+        self.assertIs(model.StudentRegistration.find(s1, st3), sr2)
+        self.assertIs(model.StudentRegistration.find(s1.sid, st3), sr2)
+        self.assertIs(model.StudentRegistration.find(s1, st3.uid), sr2)
+        self.assertRaises(model.ForeignKeyNotMapped,
+                model.StudentRegistration.find, 10, 10)
+        self.assertRaises(model.NotFound,
+                model.StudentRegistration.find, s2, st2)
+        self.assertIs(model.StudentRegistration.get(sr3.srid), sr3)
+
+    def testGet(self):
+        self.assertRaises(ValueError, model.StudentRegistration.get, 'foo')
+
+class MiscTestCase(EnseignerTestCase):
+    def testSession(self):
+        s1 = model.Session.create('foo', 'bar')
+        s2 = model.Session.create('foo2', 'bar2')
+        self.assertEqual(set(model.Session.all()), {s1, s2})
+
+    def testSubject(self):
+        self.assertRaises(ValueError, model.Subject.get, 'foo')
